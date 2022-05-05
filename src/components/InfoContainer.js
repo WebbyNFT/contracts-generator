71a5c252147cac431e94256886926532bf5e48a3
WebbyNft <webbynft@gmail.com>
2022-05-05 12:55:13 +0200

    feat: add info

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -4,13 +4,21 @@ import React from "react";
 export default function InfoContainer() {
   return (
     <>
-      <Flex p="4" justifyContent="space-between">
-        <Flex>
-          Author:{" "}
-          <Link href="https://github.com/WebbyNFT" isExternal ml="1">
-            Webby NFT
-          </Link>
-        </Flex>
+      <Flex p="4" justifyContent="space-between" alignItems="center">
+        <Grid>
+          <Text>
+            Author:{" "}
+            <Link
+              href="https://github.com/WebbyNFT"
+              isExternal
+              ml="1"
+              color="blue.500"
+            >
+              Webby NFT
+            </Link>
+          </Text>
+          <Text>Discord: Webby#6345</Text>
+        </Grid>
         <Text>Used: 512 times</Text>
       </Flex>
     </>
