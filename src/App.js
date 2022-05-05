a8eaee762845c095c37974298ca93ec3c8758b62
WebbyNft <webbynft@gmail.com>
2022-05-05 13:10:19 +0200

    fix: remove unnecessary code, files and libraries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.scss";
 import Form from "./components/Form";
 import Result from "./components/Result";
 import { ContractDataContext } from "./utils/contexts";
